9943f6ebcd1e2426738c235c62576b60b41ca624
HIRU <rlawoehd187@naver.com>
2019-07-20 17:03:09 +0900

    mistake

--- aosip.mk
+++ aosip.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/pantech/aosp_ef63.mk)
+$(call inherit-product, device/pantech/ef63/aosp_ef63.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosip_ef63
