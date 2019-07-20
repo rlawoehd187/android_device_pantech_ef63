db4e1456912d89dd0776ac6d276b6d73f27e7d65
Hiru <rlawoehd187@naver.com>
2019-07-20 16:07:06 +0900

    Initial AOSiP Bringup

---
+++ aosip.mk
@@ -0,0 +1,20 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common aosip stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/pantech/aosp_ef63.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aosip_ef63
+PRODUCT_BRAND := pantech
+PRODUCT_MODEL := VEGA IRON 2
+PRODUCT_MANUFACTURER := PANTECH
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ef63-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys" 
+
+BUILD_FINGERPRINT := pantech/ef63:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys
